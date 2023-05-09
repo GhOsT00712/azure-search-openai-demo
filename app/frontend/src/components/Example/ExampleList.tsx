864530056f98f8505c1cc14d57feb14db0453c62
GhOsT <get.piyush007@gmail.com>
2023-05-09 14:42:13 +0000

    changes with UI prompts

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What are M365 Groups?",
+        value: "What are M365 Groups?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "How to delete a Public Folder?", value: "How to delete a Public Folder?" },
+    { text: "What to disable Guests invite for a Group?", value: "What to disable Guests invite for a Group?" }
 ];
 
 interface Props {
